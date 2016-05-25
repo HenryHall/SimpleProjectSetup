6b9a72375184dd0b6803046300885476b6ca5426
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-05-24 22:27:24 -0500

    Doge!

---
+++ Scripts/myJavascript.js
@@ -0,0 +1,5 @@
+function clicked() {
+  console.log("'Woof!' (The button has been clicked!)");
+  document.getElementById('woof').innerHTML ="Woof!";
+  document.getElementById("doge").className ="clicked";
+}
